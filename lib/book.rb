2f57d2c60b3473e0c053195a46171e0a59995854
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-08 21:21:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,17 @@
-class Book
-  attr_accessor :author, :page_count
-  attr_reader :title
+# class Book
+#   attr_accessor :author, :page_count
+#   attr_reader :title
 
-  GENRES = []
-  def initialize(title)
-    @title = title
-  end
+#   GENRES = []
+#   def initialize(title)
+#     @title = title
+#   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-def genre=(genre)
-  @genre = genre
-  GENRES << genre
-end
-end
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# def genre=(genre)
+#   @genre = genre
+#   GENRES << genre
+# end
+# end
